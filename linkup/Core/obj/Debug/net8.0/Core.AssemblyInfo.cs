17c0eb9a5f2db7e22ecf6415ea492466f75fcfe4
Thiranjaya Siriwardhana <kanujathiranjaya@gmail.com>
2026-05-23 15:26:08 +0530

    Update assembly information and add User entity class

--- linkup/Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ linkup/Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8b32408ad54b143b00b4057138b4eca184b95d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
