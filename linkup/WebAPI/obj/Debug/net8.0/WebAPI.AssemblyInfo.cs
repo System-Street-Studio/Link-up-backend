96721e63fc4950103f3f4400fbaa135ecaa8af04
Thiranjaya Siriwardhana <kanujathiranjaya@gmail.com>
2026-05-28 15:24:53 +0530

    Update assembly informational version and related cache files

--- linkup/WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ linkup/WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17c0eb9a5f2db7e22ecf6415ea492466f75fcfe4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d72007262a2234c7bafeb5243b66c0c93044853d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
