e35349334d8e1e5a9ae5be0ec6f580a7d3c68e5d
Thiranjaya Siriwardhana <kanujathiranjaya@gmail.com>
2026-05-28 16:10:43 +0530

    Add project.nuget.cache file for dependency management

--- linkup/Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ linkup/Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93b0d71cbb6bf4e163dfe0e3a52489ae40ade9af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30773d0246edd05e5181ee6422ac0d82fbf630da")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
